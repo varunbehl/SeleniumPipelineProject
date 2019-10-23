2d24b86cbb9c369b7ac91c9c1b977adb16780233
Behl <varun.behl@capgemini.com>
2019-10-23 14:59:49 +0530

    Initial Commit

---
+++ Pipeline1/src/main/java/RegressionTests/LoginTest.java
@@ -0,0 +1,21 @@
+package RegressionTests;
+
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+
+
+
+    public static void main(String args[]){
+
+        testlogin();
+
+    }
+    @Test
+    public static void testlogin(){
+
+        System.out.println("This is the login test script");
+    }
+}
